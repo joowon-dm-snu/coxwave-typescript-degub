6bf1c4c3150cba10e7c19950da3d0745b9af4bbc
Joowon <joowon.kim@dm.snu.ac.kr>
2023-01-31 18:23:01 +0900

    chore: setup minimal jest (#7)

---
+++ packages/analytics-browser/test/browser-client.test.ts
@@ -0,0 +1,7 @@
+describe('browser-client', () => {
+  describe('init', () => {
+    test('pass this test', () => {
+      expect(1).toBe(1);
+    });
+  });
+});
---
+++ packages/analytics-client-common/test/client-common.test.ts
@@ -0,0 +1,7 @@
+describe('client-common', () => {
+  describe('init', () => {
+    test('pass this test', () => {
+      expect(1).toBe(1);
+    });
+  });
+});
