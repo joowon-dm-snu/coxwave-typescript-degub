bb69fbc0d2c9dcb147d768720a866826b613ebde
joowon-dm-snu <joowon.kim@dm.snu.ac.kr>
2023-02-03 15:53:42 +0900

    init

--- packages/analytics-core/src/constants.ts
+++ packages/analytics-core/src/constants.ts
@@ -1,10 +1,10 @@
 export const UNSET_VALUE = '-';
 export const COXWAVE_PREFIX = 'COX';
 export const STORAGE_PREFIX = `${COXWAVE_PREFIX}_unsent`;
-export const COXWAVE_SERVER_URL = 'https://ingest-dev.coxwave.com'; // TODO: will be changed later
-export const EU_COXWAVE_SERVER_URL = 'https://ingest-dev.coxwave.com'; // TODO: will be changed later
-export const COXWAVE_BATCH_SERVER_URL = 'https://ingest-dev.coxwave.com'; // TODO: will be changed later
-export const EU_COXWAVE_BATCH_SERVER_URL = 'https://ingest-dev.coxwave.com'; // TODO: will be changed later
+export const COXWAVE_SERVER_URL = 'http://localhost:8080'; // TODO: will be changed later
+export const EU_COXWAVE_SERVER_URL = 'http://localhost:8080'; // TODO: will be changed later
+export const COXWAVE_BATCH_SERVER_URL = 'http://localhost:8080'; // TODO: will be changed later
+export const EU_COXWAVE_BATCH_SERVER_URL = 'http://localhost:8080'; // TODO: will be changed later
 
 export const SERVER_ACTIVITIES_PATH = '/events/activities';
 export const SERVER_GENERATIONS_PATH = '/events/generations';
